40e43aaa3d5d153c6ab194d7b00d7c2718c6a8ca
翁雪松 <wengxuesong@vcomic.com>
2018-06-22 16:49:24 +0800

    4-5

---
+++ 4-5/4-5/main.m
@@ -0,0 +1,36 @@
+//
+//  main.m
+//  4-5
+//
+//  Created by 翁雪松 on 2018/6/22.
+//  Copyright © 2018年 翁雪松. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        float f1 = 123.125, f2;
+        int i1,i2 = -150;
+        
+        i1 = f1;
+        NSLog(@"%f assigned to an int produces %i",f1,i1);
+        
+        f1 = i2;
+        NSLog(@"%i assigned to a float produces %f", i2, f1);
+        
+        f1 = i2 / 100;
+        NSLog(@"%i divided by 100 produces %f",i2,f1);
+        
+        f2 = i2 / 100.0;
+        NSLog(@"%i divided by 100.0 produces %f",i2, f2);
+        
+        f2 = (float) i2 / 100;
+        NSLog(@"(float) %i divided by 100 produces %f", i2, f2);
+        
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
