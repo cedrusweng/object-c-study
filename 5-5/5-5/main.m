9a7cbb36fcd89ae6c06f262d26f1dace8a67144a
翁雪松 <wengxuesong@vcomic.com>
2018-06-25 11:03:49 +0800

    5-5

---
+++ 5-5/5-5/main.m
@@ -0,0 +1,29 @@
+//
+//  main.m
+//  5-5
+//
+//  Created by 翁雪松 on 2018/6/25.
+//  Copyright © 2018年 cedrus.weng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int n, number,triangularNumber,counter;
+        
+        for (counter = 1; counter <=5;++counter){
+            NSLog(@"What triangular number do you want?");
+            scanf("%i",&number);
+            
+            triangularNumber = 0;
+            
+            for (n=1;n<=number;++n){
+                triangularNumber += n;
+            }
+            
+            NSLog(@"Triangular number %i is %i",number,triangularNumber);
+        }
+    }
+    return 0;
+}
