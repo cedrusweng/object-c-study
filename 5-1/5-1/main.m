5faa6fda05585a0e89f4ee859b29f230485eaa9c
翁雪松 <wengxuesong@vcomic.com>
2018-06-25 10:23:41 +0800

    for

---
+++ 5-1/5-1/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  5-1
+//
+//  Created by 翁雪松 on 2018/6/25.
+//  Copyright © 2018年 cedrus.weng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int triangularNumber;
+        
+        triangularNumber = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+        
+        // insert code here...
+        NSLog(@"The eighth triangular number is %i.",triangularNumber);
+    }
+    return 0;
+}
---
+++ 5-2/5-2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  5-2
+//
+//  Created by 翁雪松 on 2018/6/25.
+//  Copyright © 2018年 cedrus.weng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int n, triangularNumber;
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
