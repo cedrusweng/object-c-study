2b2131ebbd25f11f9ea054ecbbf202ad7d088b3c
翁雪松 <wengxuesong@vcomic.com>
2018-06-25 10:26:34 +0800

    5-2

--- 5-2/5-2/main.m
+++ 5-2/5-2/main.m
@@ -11,8 +11,15 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         int n, triangularNumber;
+        
+        triangularNumber = 0;
+        
+        for ( n = 1; n <= 200;n = n + 1) {
+            triangularNumber += n;
+        }
+        
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"The 200th triangular number is %i", triangularNumber);
     }
     return 0;
 }
