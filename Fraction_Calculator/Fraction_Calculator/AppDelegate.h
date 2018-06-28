6907e1ac4212be5ece2dd0770ce4abf7a54bee5b
翁雪松 <wengxuesong@vcomic.com>
2018-06-28 18:45:09 +0800

    Fraction_Calculator

---
+++ Fraction_Calculator/Fraction_Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fraction_Calculator
+//
+//  Created by cedrus on 2018/6/28.
+//  Copyright © 2018年 cedrus.weng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fraction_Calculator/Fraction_Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fraction_Calculator
+//
+//  Created by cedrus on 2018/6/28.
+//  Copyright © 2018年 cedrus.weng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
